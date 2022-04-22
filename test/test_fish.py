e409579c37e5444209f9490190cc4f0da0fb0add
Alex Rubinsteyn <alex.rubinsteyn@gmail.com>
2022-04-22 17:07:35 -0400

    added fish tests

--- test/test_fish.py
+++ test/test_fish.py
@@ -1,4 +1,4 @@
-from mhcgnomes import parse, Allele, Gene, Species
+from mhcgnomes import parse, Allele, Gene, Species, Pair
 from nose.tools import eq_
 
 def test_parse_trout_species_Onmy():
@@ -26,3 +26,29 @@ def test_parse_trout_allele_Onmy_DAB_050101():
     expected = Allele.get("Onmy", "DAB", ["05", "01", "01"])
     assert expected is not None
     eq_(result, expected)
+
+def test_parse_grass_carp_B2M_1_ii_star_sep():
+    expected = Allele.get("Ctid", "B2M-1", "ii")
+    result = parse("Ctid-B2M-1*ii")
+    eq_(result, expected)
+
+
+def test_parse_grass_carp_B2M_1_ii_dash_sep():
+    expected = Allele.get("Ctid", "B2M-1", "ii")
+    result = parse("Ctid-B2M-1-ii")
+    eq_(result, expected)
+
+def test_parse_grass_carp_mhc1_pair_UAA_B2M_1_ii_dash_sep():
+    expected = Pair(
+        alpha=Gene.get("Ctid", "UAA"),
+        beta=Allele.get("Ctid", "B2M-1", "ii"))
+    result = parse("Ctid-UAA/B2M-1-ii")
+    eq_(result, expected)
+
+def test_parse_grass_carp_mhc1_pair_UAA_B2M_1_ii_star_sep():
+    expected = Pair(
+        alpha=Gene.get("Ctid", "UAA"),
+        beta=Allele.get("Ctid", "B2M-1", "ii"))
+    result = parse("Ctid-UAA/B2M-1*ii")
+    eq_(result, expected)
+
