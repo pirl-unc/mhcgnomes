38cdbaad2394b7fe5e3034821a469fd5f963963f
Alex Rubinsteyn <alex.rubinsteyn@gmail.com>
2022-05-06 18:43:22 -0400

    fixing tests

--- test/test_human.py
+++ test/test_human.py
@@ -134,7 +134,7 @@ def test_parse_human_class2_DRB1_01_02():
 
 
 def test_normalized_string_human_class2_DRB1_01_02():
-    expected = "HLA-DRA*01:01-DRB1*01:02"
+    expected = "HLA-DRA*01:01/DRB1*01:02"
     for name in ["DRB1_0102",
                  "DRB101:02",
                  "HLA-DRB1_0102",
@@ -171,7 +171,7 @@ def test_parse_human_class2_alpha_beta_DPA1_01_05_DPB1_100_01_full_syntax_hyphen
         Allele.get("HLA", "DPA1", "01", "05"),
         Allele.get("HLA", "DPB1", "100", "01")
     )
-    s = "HLA-DPA1*01:05-DPB1*100:01"
+    s = "HLA-DPA1*01:05/DPB1*100:01"
     eq_(parse(s, infer_class2_pairing=True), expected)
 
 def test_parse_human_class2_alpha_beta_DPA1_01_05_DPB1_100_01_full_syntax_hyphens_lower():
@@ -224,7 +224,7 @@ def test_parse_all_parameters_true_human_class2_alpha_beta_DPA1_01_05_DPB1_100_0
         eq_(parse_result, expected)
 
 def test_normalize_string_human_class2_alpha_beta_DPA1_01_05_DPB1_100_01():
-    expected = "HLA-DPA1*01:05-DPB1*100:01"
+    expected = "HLA-DPA1*01:05/DPB1*100:01"
     for name in ["DPA10105-DPB110001",
                  "HLA-DPA1*01:05-DPB1*100:01",
                  "hla-dpa1*0105-dpb1*10001",
@@ -235,7 +235,7 @@ def test_normalize_string_human_class2_alpha_beta_DPA1_01_05_DPB1_100_01():
 
 
 def test_compact_string_human_class2_alpha_beta_DPA1_01_05_DPB1_100_01():
-    expected_compact = "DPA1*0105-DPB1*10001"
+    expected_compact = "DPA1*0105/DPB1*10001"
     for name in ["DPA10105-DPB110001",
                  "HLA-DPA1*01:05-DPB1*100:01",
                  "hla-dpa1*0105-dpb1*10001",
@@ -248,7 +248,7 @@ def test_compact_string_human_class2_alpha_beta_DPA1_01_05_DPB1_100_01():
 def test_alpha_chain_inference_DP():
     # example of common DP haplotype from wikipedia article on HLA-DP
     just_beta = "HLA-DPB1*04:01"
-    expected = "HLA-DPA1*01:03-DPB1*04:01"
+    expected = "HLA-DPA1*01:03/DPB1*04:01"
     eq_(parse(just_beta, infer_class2_pairing=True).to_string(), expected)
     eq_(parse(just_beta, infer_class2_pairing=False).to_string(), just_beta)
 
@@ -256,7 +256,7 @@ def test_alpha_chain_inference_DP():
 def test_alpha_chain_inference_DQ():
     # example of common DQ haplotype from wikipedia article on HLA-DQ
     just_beta = "HLA-DQB1*06:02"
-    expected = "HLA-DQA1*01:02-DQB1*06:02"
+    expected = "HLA-DQA1*01:02/DQB1*06:02"
     eq_(parse(just_beta, infer_class2_pairing=True).to_string(), expected)
     eq_(parse(just_beta, infer_class2_pairing=False).to_string(), just_beta)
 
