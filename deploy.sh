0bed83500124bdb28d8369922b1ac2a466172d77
Alex Rubinsteyn <alex.rubinsteyn@gmail.com>
2024-03-27 00:51:34 -0400

    added tags to deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,10 @@
-rm -rf dist
-python3 setup.py bdist_wheel
-python3 setup.py sdist
-twine upload dist/*whl dist/*gz
+./lint.sh && \
+./test.sh && \
+python3 -m pip install --upgrade build && \
+python3 -m pip install --upgrade twine && \
+rm -rf dist && \
+python3 -m build && \
+git --version && \
+python3 -m twine upload dist/* && \
+git tag "$(python3 mhcgnomes/version.py)" &&  \
+git push --tags
